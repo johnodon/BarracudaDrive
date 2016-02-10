8ea919d4f72ba89183e90bef5656f92acc4961fb
johnodon <johnodon@hotmail.com>
2016-02-10 08:51:34 -0500

    Update install.sh

--- init/install.sh
+++ init/install.sh
@@ -2,9 +2,9 @@
 cd /tmp
 
 # If BD is already installed the upgrade
-if [ -f "/home/bd/bdd" ]; then
+if [ -f "/home/nobody/bdd" ]; then
 	sleep 1
-	chown -R bd:daemon /home/bd
+	chown -R nobody:users /home/nobody
 	rm -f bd&&mkdir bd&&cd bd
 	wget http://barracudadrive.com/BarracudaDrive.linux-x64.tar.gz || abort
 	tar xvzf BarracudaDrive.linux-x64.tar.gz
@@ -16,34 +16,37 @@ if [ -f "/home/bd/bdd" ]; then
     	rm -f Config-Wizard.zip
     	wget http://barracudadrive.com/unix/backup.zip || abort
     	cd ..
-    	chown -R bd:daemon ./*
+    	chown -R nobody:users ./*
     	/etc/init.d/bdd stop
     	sleep 2
     	mv bdd.conf bdd.conf.NEW
-    	cp -p -r ./* /home/bd
+    	cp -p -r ./* /home/nobody
     	cd ..
     	rm -rf bd
     	/etc/init.d/bdd start
     	exit 0
 fi
 
-useradd -G daemon -m -s /bin/bash  bd || abort
-chown -R bd:daemon /home/bd
-cd /home/bd || abort
+useradd -G users -m -s /bin/bash nobody || abort
+chown -R nobody:users /home/nobody
+cd /home/nobody || abort
 wget http://barracudadrive.com/BarracudaDrive.linux-x64.tar.gz || abort
 tar xvzf BarracudaDrive.linux-x64.tar.gz || abort
 chmod +x bdd
 rm -f BarracudaDrive.linux-x64.tar.gz
 rm -f *.txt
 cd InstallDaemon || abort
+sed -i 's/BDUSER=bd/BDUSER=nobody/g' /home/nobody/InstallDaemon/bdd.sh
+sed -i 's/BDHOME=#home#bd/BDUSER=#home#nobody/g' /home/nobody/InstallDaemon/bdd.sh
 chmod +x *.sh
 ./install.sh
+sleep 5
 cd ..
 rm -rf InstallDaemon || abort
-ln -s /home/bd/cmsdocs disk/cmsdocs
-ln -s /home/bd/applications disk/applications
+ln -s /home/nobody/cmsdocs disk/cmsdocs
+ln -s /home/nobody/applications disk/applications
 cd applications
 wget http://barracudadrive.com/unix/sslcert.zip || abort
-chown -R bd:daemon /home/bd
+chown -R nobody:users /home/nobody
 /etc/init.d/bdd start
 exit 0
