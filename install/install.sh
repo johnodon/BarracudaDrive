3ac5c1e0f2ba9c8b80cfbde95c4b3bf037d1f859
johnodon <johnodon@hotmail.com>
2016-02-10 05:12:19 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 cd /tmp
+
+# If BD is already installed the upgrade
 if [ -f "/home/bd/bdd" ]; then
 	sleep 1
-    rm -f bd&&mkdir bd&&cd bd
+	rm -f bd&&mkdir bd&&cd bd
 	wget http://barracudadrive.com/BarracudaDrive.linux-x64.tar.gz || abort
 	tar xvzf BarracudaDrive.linux-x64.tar.gz
-    rm BarracudaDrive.linux-x64.tar.gz
-    chmod +x bdd
-    #Remove files that should not be upgraded
-    rm -rf cmsdocs data disk InstallDaemon
-    cd applications
-    rm -f Config-Wizard.zip
-    wget http://barracudadrive.com/unix/backup.zip || abort
-    cd ..
-    chown -R bd:daemon ./*
-    /etc/init.d/bdd stop
-    sleep 2
-	mv bdd.conf bdd.conf.NEW
-    cp -p -r ./* /home/bd
-    cd ..
-    rm -rf bd
-    /etc/init.d/bdd start
-    exit 0
+	rm BarracudaDrive.linux-x64.tar.gz
+    	chmod +x bdd
+    	#Remove files that should not be upgraded
+    	rm -rf cmsdocs data disk InstallDaemon
+    	cd applications
+    	rm -f Config-Wizard.zip
+    	wget http://barracudadrive.com/unix/backup.zip || abort
+    	cd ..
+    	chown -R bd:daemon ./*
+    	/etc/init.d/bdd stop
+    	sleep 2
+    	mv bdd.conf bdd.conf.NEW
+    	cp -p -r ./* /home/bd
+    	cd ..
+    	rm -rf bd
+    	/etc/init.d/bdd start
+    	exit 0
 fi
 
 useradd -G daemon -m -s /bin/bash  bd || abort
@@ -42,3 +44,4 @@ cd applications
 wget http://barracudadrive.com/unix/sslcert.zip || abort
 chown -R bd:daemon /home/bd
 /etc/init.d/bdd start
+exit 0
